0cc551861cc258f6a9a51db15934002679ab0cd6
Anubhab Sen <anubhabsen@gmail.com>
2017-06-07 17:22:57 +0530

    initial commit

---
+++ image_graph.m
@@ -0,0 +1,29 @@
+I = imread('image.jpg') ;
+I = single(vl_imdown(rgb2gray(I))) ;
+
+binSize = 8;
+magnif = 3;
+Is = vl_imsmooth(I, sqrt((binSize/magnif)^2 - .25));
+
+[f, d] = vl_dsift(Is, 'size', binSize);
+
+N = size(f, 2);
+
+weighted_graph(1:N, 1:N) = 0;
+
+for i = 1:N
+    for j = 1:N
+        weighted_graph(i, j) = pdist(transpose(horzcat(d(:, i), d(:, j))));
+    end
+end
+
+D(1:N, 1:N) = 0;
+
+for i = 1:N
+    sum = 0;
+    for j = 1:N
+        sum = sum + weighted_graph(i, j);
+    end
+    D(i, i) = sum;
+end
+
